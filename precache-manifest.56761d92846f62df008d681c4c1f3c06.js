4976f70dd3c522cbccc524216aa1509185f8a559
supra321 <suprativdatta@gmail.com>
2020-05-01 23:38:35 +0530

    Updates

---
+++ precache-manifest.56761d92846f62df008d681c4c1f3c06.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44eb25958d672e92e9da769d8d0f3823",
+    "url": "/tourlist-redux-pwa/index.html"
+  },
+  {
+    "revision": "cb5d9647633f1146ae63",
+    "url": "/tourlist-redux-pwa/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "4a2545046051b4bf714f",
+    "url": "/tourlist-redux-pwa/static/css/main.d94b9b00.chunk.css"
+  },
+  {
+    "revision": "cb5d9647633f1146ae63",
+    "url": "/tourlist-redux-pwa/static/js/2.f12a31f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tourlist-redux-pwa/static/js/2.f12a31f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a2545046051b4bf714f",
+    "url": "/tourlist-redux-pwa/static/js/main.600d59c4.chunk.js"
+  },
+  {
+    "revision": "d955605501b5c4dc1d0a",
+    "url": "/tourlist-redux-pwa/static/js/runtime-main.89243a16.js"
+  },
+  {
+    "revision": "56f56c319f80758c6a68ad4be0ca5bc8",
+    "url": "/tourlist-redux-pwa/static/media/futura-pt-medium-589e45b956de4.56f56c31.woff"
+  }
+]);
